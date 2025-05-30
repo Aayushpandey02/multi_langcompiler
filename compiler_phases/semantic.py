14e45fc6c404e80aec1bc99486217be9d092f4af
Harshvardhan Ojha <hannuojha007@gmail.com>
2025-05-30 06:26:22 +0530

    final semantic

--- compiler_phases/semantic.py
+++ compiler_phases/semantic.py
@@ -1,3 +1,10 @@
 class SemanticError(Exception):
     pass
 
+def analyze_semantics(ast, lang):
+    # Simple semantic check example: undefined variable check
+    # For demo, if token 'undefined_var' present, error
+    if 'undefined_var' in ast:
+        raise SemanticError("Use of undefined variable 'undefined_var'")
+
+
