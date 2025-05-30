5302acf449953f92deb00267dcd2e44f019a6d20
Harshvardhan Ojha <hannuojha007@gmail.com>
2025-05-30 06:24:54 +0530

    parser final

--- compiler_phases/parser.py
+++ compiler_phases/parser.py
@@ -3,5 +3,19 @@ class SyntaxError(Exception):
 
 def parse(tokens, lang):
     # Very basic parser: check for balanced brackets as a demo
-   
+    stack = []
+    brackets = {'(': ')', '{': '}', '[': ']'}
+    for token in tokens:
+        if token in brackets.keys():
+            stack.append(token)
+        elif token in brackets.values():
+            if not stack:
+                raise SyntaxError(f"Unmatched closing bracket '{token}'")
+            open_bracket = stack.pop()
+            if brackets[open_bracket] != token:
+                raise SyntaxError(f"Mismatched brackets '{open_bracket}' and '{token}'")
+    if stack:
+        raise SyntaxError(f"Unclosed bracket '{stack[-1]}'")
+    # Return a dummy AST (just the token list here)
     return tokens
+
